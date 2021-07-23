0319f22178a839c59081730bed009ad8a8329c60
Allanlimo02 <allan.limo@student.moringaschool.com>
2021-07-23 15:26:19 +0300

    Added jquerry to hide and show design section

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $("div.info1").click(function(){
+        $('.info1').hide()
+        $('#info1').show()
+    })
+    $("div#info1").click(function(){
+        $('.info1').show()
+        $('#info').hide(    )
+    })
+})
